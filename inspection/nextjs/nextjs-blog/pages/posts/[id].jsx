7c5ed61a89a321981a1a09034a3d77f736a018f9
福島貴裕 <fukushimatakahiro@fukku.local>
2022-06-28 20:25:47 +0900

    nextjs tutorial init~

---
+++ inspection/nextjs/nextjs-blog/pages/posts/[id].jsx
@@ -0,0 +1,19 @@
+import Layout from "../../components/Layout";
+
+import { getAllPostIds } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post() {
+  return (
+    <Layout>
+      <h1>Post</h1>
+    </Layout>
+  );
+}
